dd57dee527f0327004551ac3e3740378010884c6
alucard <LeroyKokoti3@gmail.com>
2026-02-18 14:52:28 +0000

    handler to return a specific item

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
-
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
@@ -47,4 +47,20 @@ func postAlbums(c *gin.Context) {
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 
+}
+
+// getAlbumByID locates the albums whose ID value matches the id
+// paramter sent by the client, then returns that album as response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	//Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
